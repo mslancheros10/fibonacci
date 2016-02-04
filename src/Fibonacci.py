4ba4470f378bfd7b7f8cea9065b36e17a090b3ea
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-02-04 14:32:14 -0500

    The task is ready!!

--- src/Fibonacci.py
+++ src/Fibonacci.py
@@ -1,11 +1,46 @@
 
-iterations = 5
-counter = 1
-x1, x2 = 0,1
+import sys
+from Tkinter import *
+import tkMessageBox
 
-while(counter<=5):
-    print x2
-    x1,x2 = x2,x1+x2
-    counter+=1
+def showFibonacci(textArea):
+    mensaje = ""
+    try:
+        print entryWidget.get()
+        iterations=int(textArea)
+        counter = 1
+        x1, x2 = 0,1
+        while(counter<=iterations):
+            mensaje = mensaje + str(x2) +" "
+            x1,x2 = x2,x1+x2
+            counter+=1
+        tkMessageBox.showinfo("The result is...", mensaje)
+    except ValueError:
+        tkMessageBox.showerror("Error", "Not a number")
+
+
+
+
+top = Tk()
+top.title("Fibonacci series")
+textFrame = Frame(top)
+
+entryLabel = Label(textFrame)
+entryLabel["text"] = "Insert the number of iterations:"
+entryLabel.pack(side=LEFT)
+
+entryWidget = Entry(textFrame)
+entryWidget["width"] = 50
+entryWidget.pack(side=LEFT)
+
+textFrame.pack()
+
+button = Button(top, text="Submit", command=lambda:showFibonacci(entryWidget.get()))
+button.pack()
+
+top.mainloop()
+
+
+
 
 
